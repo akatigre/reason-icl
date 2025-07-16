facaa4afd0200b13e2d4b0fe6671b3da972f1f9a
akatigre <yoonkim313@kaist.ac.kr>
2025-07-16 22:39:57 +0900

    data embed

--- data_embed.py
+++ data_embed.py
@@ -1,10 +1,12 @@
+import os
 import json
-from datasets import load_dataset
+import pickle
+from tqdm import tqdm
+from datasets import load_dataset, load_from_disk, Array2D
 from sentence_transformers import SentenceTransformer
 
-
-def save_into_jsonl():
-    with open("data/AOKVQA/val.json", "r") as f:
+def save_into_jsonl(data_path_json):
+    with open(data_path_json, "r") as f:
         data = json.load(f)
         
     lines = []
@@ -22,36 +24,70 @@ def save_into_jsonl():
                 "cot_wo_tag_negatives": line["cot_wo_tag_variants"]["EntitySwap"] + line["cot_wo_tag_variants"]["Paraphrasing"]
             })
     
-    with open("data/AOKVQA/val.jsonl", "w") as f:
+    with open(data_path_json.replace(".json", ".jsonl"), "w") as f:
         for line in lines:
             f.write(json.dumps(line) + "\n")
 
-
-if __name__ == "__main__":
-    ds = load_dataset("json", data_files="data/AOKVQA/val.jsonl")
-
-    model = SentenceTransformer("Qwen/Qwen3-Embedding-4B", model_kwargs={"torch_dtype": "bfloat16"})
+def embed_and_save_faiss(ds, save_to_path, emb_model, gen_model):
+    model = SentenceTransformer(emb_model, model_kwargs={"torch_dtype": "bfloat16"})
 
     # keys_to_embed = ["solution", ""]
     pool = model.start_multi_process_pool(target_devices=["cuda:0", "cuda:1"])
     # Encode with multiple GPUs
-    keys_to_embed = ["cot_wo_tag", "cot_wo_tag_negatives", "question"]
+    keys_to_embed = ["cot_wo_tag", "question"]
+    embeddings = {}
     for key in keys_to_embed:
-        if isinstance(ds[key], list):
-            inputs_nested_list = [line[key] for line in ds]
-            inputs_list = None
-        else:
-            inputs_list = [line[key] for line in ds]
-        if inputs_list is not None:
-            embeddings = model.encode(inputs_list, pool=pool)
-        else:
-            embeddings_list = [model.encode(list_of_strings) for list_of_strings in inputs_nested_list]
-
-        for line in ds:
-            ds[""]
+        key_to_save = key + "_emb"
+        inputs_list = [line[key] for line in ds]
+        
+        embeddings = model.encode(inputs_list, pool=pool)
+        ds = ds.add_column(key_to_save, embeddings.tolist())
+        ds.add_faiss_index(column=key_to_save)
+        ds.save_faiss_index(key_to_save, os.path.join(save_to_path, key + ".faiss"))
+    
+    inputs_nested_list = [line["cot_wo_tag_negatives"] for line in ds]
+    embedding_list = []
+    for list_of_strings in tqdm(inputs_nested_list):
+        embeddings = model.encode(list_of_strings, pool=pool)
+        embedding_list.append(embeddings.tolist())
     model.stop_multi_process_pool(pool)
     
+    M, D = len(embedding_list[0]), len(embedding_list[0][0])
     
-    ds_with_embeddings.save_faiss_index('embeddings', 'my_index.faiss')
-    ds = load_dataset('crime_and_punish', split='train[:100]')
-    ds.load_faiss_index('embeddings', 'my_index.faiss')
+    key_to_save = "cot_wo_tag_negatives_emb"
+    ds = ds.add_column(key_to_save, embedding_list)
+    ds = ds.cast_column(key_to_save, Array2D(shape=(M, D), dtype="float32"))
+    ds.save_to_disk(os.path.join(save_to_path, gen_model.split("/")[-1]))
+
+    
+def load_data_and_embeddings(save_to_path, gen_model, emb_model):
+    path_to_dataset = os.path.join(save_to_path, gen_model.split("/")[-1])
+    ds = load_from_disk(path_to_dataset)
+    path_to_emb = os.path.join(save_to_path, emb_model.split("/")[-1])
+    faiss_keys = ["question.faiss", "cot_wo_tag.faiss"]
+    emb_paths = [os.path.join(path_to_emb, key) for key in faiss_keys]
+    for faiss_key, emb_path in zip(faiss_keys, emb_paths):
+        key_name = faiss_key.replace(".faiss", "")+"_emb"
+        ds.add_faiss_index(column=key_name)
+        ds.load_faiss_index(key_name, emb_path)
+    ds = ds.with_format("numpy") # converts list in list to numpy array
+    return ds
+
+if __name__ == "__main__":
+    # data_path_json = "data/AOKVQA/val.json"
+    # save_into_jsonl(data_path_json)
+    # gen_model = "Qwen/Qwen2.5-7B-Instruct"
+    # data_path_jsonl = f"data/AOKVQA/{gen_model.split('/')[-1]}/val.jsonl"
+    # emb_model = "Qwen/Qwen3-Embedding-4B"
+    # save_to_path = f"data/AOKVQA/{emb_model.split('/')[-1]}"
+    # ds = load_dataset("json", data_files=data_path_jsonl)["train"]
+    # embed_and_save_faiss(ds, save_to_path, emb_model)
+    data_path_jsonl = f"data/AOKVQA/Qwen2.5-7B-Instruct/val.jsonl"
+    emb_model = "Qwen/Qwen3-Embedding-4B"
+    gen_model = "Qwen/Qwen2.5-7B-Instruct"
+    save_to_path = f"data/AOKVQA/"
+    ds = load_dataset("json", data_files=data_path_jsonl)["train"]
+    embed_and_save_faiss(ds, save_to_path, emb_model, gen_model)
+    ds = load_data_and_embeddings(save_to_path, gen_model, emb_model)
+    
+    
--- data_template.py
+++ data_template.py
@@ -38,19 +38,49 @@ DATA_MAP = {
     #     "subset_name": "main",
     #     "data_processor": lambda example: {'reason_answer': example["solution"] + " The answer is \\boxed{" + example["answer"] + "}"},
     # },
-    # "aokvqa": {
-    #     "template": PromptTemplate(
-    #         template=IN_CONTEXT_EXAMPLE_TOKEN + qwenvl_template,
-    #         column_token_map=in_context_placeholder_token_map,
-    #         ice_token=IN_CONTEXT_EXAMPLE_TOKEN,
-    #     ),
-    #     "input_columns": ["question", "image"],
-    #     "output_column": "reason_answer",
-    #     "train_split": "train",
-    #     "test_split": "test",
-    #     "data_processor": lambda example, split: {
-    #         "question": f"{example['question']}. Answer from one of the choices {example['choices']}. When the provided information is insufficient, respond with 'Unanswerable'.\nAnswer the question using a single word or phrase.",
-    #         "image": get_coco_path(split, example['image_id'], "./data/coco"), 
-    #         "answer": example['choices'][ example['correct_choice_idx'] ] if split == "train" else example["difficult_direct_answer"]}
-    # }
+    "aokvqa": {
+        "template": PromptTemplate(
+            template=IN_CONTEXT_EXAMPLE_TOKEN + qwenvl_template,
+            column_token_map=in_context_placeholder_token_map,
+            ice_token=IN_CONTEXT_EXAMPLE_TOKEN,
+        ),
+        "input_columns": ["question", "image"],
+        "output_column": "reason_answer",
+        "train_split": "train",
+        "test_split": "val",
+        "data_path": "openai/gsm8k",
+        "data_processor": lambda example, split: {
+            "question": f"{example['question']}. Answer from one of the choices {example['choices']}. When the provided information is insufficient, respond with 'Unanswerable'.\nAnswer the question using a single word or phrase.",
+            "image": get_coco_path(split, example['image_id'], "./data/coco"), 
+            "answer": example['choices'][ example['correct_choice_idx'] ] if split == "train" else example["difficult_direct_answer"]}
+    }
 }
+
+from utils.aokvqa_utils import load_aokvqa, get_coco_path
+aokvqa_dir = "./data/aokvqa/"
+assert args.split in ["val", "train"]
+dataset = load_aokvqa(aokvqa_dir, args.split)
+if isinstance(dataset, list):
+    dataset = {dataset[i]['question_id'] : dataset[i] for i in range(len(dataset)) }
+    
+image_id = data['image_id']    
+image_path = get_coco_path(split, image_id, coco_dir)
+
+question = data['question']
+choices = data['choices']
+if split == "test":
+    question_id = data['question_id']
+    correct_choice_idx = None
+    correct_choice = None
+    direct_diffucult = data['difficult_direct_answer']
+    meta_data = {
+        "question_id": question_id,
+        "direct_diffucult": direct_diffucult
+    }
+else:
+    correct_choice_idx = data['correct_choice_idx']
+    correct_choice = data['choices'][ correct_choice_idx ]
+    meta_data = {
+        # "rationales": rationales,
+        "answer": correct_choice
+    }
